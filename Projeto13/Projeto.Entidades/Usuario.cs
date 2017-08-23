e9e048b20f21f25844299d0f0e97e68d040abafc
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-08-22 23:28:00 -0300

    Entidade em desenvolvimento!

---
+++ Projeto13/Projeto.Entidades/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Usuario
+    {
+        //Atributo
+        private int idUsuario;
+        private string nome;
+        private string login;
+        private string senha;
+        private string foto;
+        private DateTime dataCadastro;
+
+        public Usuario()
+        {
+
+        }
+    }
+}
